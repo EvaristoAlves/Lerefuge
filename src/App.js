dd2bcdac6d9e5ecc9791cc647190cc22653583c6
Evaristo Alves <evaristocarvalho34@gmail.com>
2022-02-01 19:07:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img href="./Imgs/Logotipo.png"></img>
+          <p>
+          Edit <code>src/App.js</code> e salve para recarregar.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a>
+          Aprendendo React!!
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+
